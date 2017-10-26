888b5c3187daa285e223bb843eb72ba5b7b6f5be
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-26 14:53:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
+var array = []
 
 function forLoop(array){
-  var array = []
-
-  for (var i = 0; i < 25; i++){
-    if( i === 1){
+    for (var i = 0; i < 25; i++){
+    if(i === 1) {
       array.push("I am 1 strange loop.")
   } else{
-    array.push("I am " + ${i} + " strange loops.")
+    array.push("I am " + `${i}` + " strange loops.")
   }
     return array
     }
