ea81f1d6d433ff3d40326a64a9504a2dc7bb72c9
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-26 14:52:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,11 +4,12 @@ function forLoop(array){
   for (var i = 0; i < 25; i++){
     if( i === 1){
       array.push("I am 1 strange loop.")
-  } else{array.push("I am " + ${i} + " strange loops.")}
-
+  } else{
+    array.push("I am " + ${i} + " strange loops.")
+  }
     return array
     }
-  }
+      }
 
 
 function whileLoop(n){
@@ -16,7 +17,7 @@ function whileLoop(n){
     console.log(--n)
   return "done"
 }
-
+}
 
 function maybeTrue() {
   return Math.random() >= 0.5
